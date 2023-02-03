0d0ec373462463e13fd75e9ab3410bffe43c3223
nikolaKnezevic00 <101424302+nikolaKnezevic00@users.noreply.github.com>
2023-02-03 14:47:56 +0100

    Implement monitoring server && refactor code

--- SecurityService.cs
+++ SecurityService.cs
@@ -1,13 +1,9 @@
 ﻿using Manager;
 using ServiceContract;
-using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Security.Cryptography;
 using System.ServiceModel;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Server
 {
